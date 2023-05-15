0e924302cf1805e2fedae2e3dcda3d3187fd9132
BhaktiDeshmukh <Bhaktideshmukh1996@gmail.com>
2023-05-15 16:24:12 +0530

    commiting after cloning

---
+++ src/test/java/com/test/verifyhomepage.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class verifyhomepage {
+
+	public static void main(String[] args) {
+		
+		System.out.println("homepage test cases");
+		
+
+	}
+
+}
--- src/test/java/com/test/verifylogintest.java
+++ src/test/java/com/test/verifylogintest.java
@@ -3,7 +3,8 @@ package com.test;
 public class verifylogintest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("login test script");
+		
 
 	}
 
